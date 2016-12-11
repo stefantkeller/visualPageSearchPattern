3d678d2dbf9cfc77798dbac0da317f0377603c8a
Stefan Keller <stefantkeller@gmail.com>
2016-12-11 14:12:24 +0100

    basic functionality to overlay a div upon activating the add-on

---
+++ gui.js
@@ -0,0 +1,17 @@
+// add a div to the page, of class vPSP
+// its appearance we can control in the .css through this class name
+var maindiv = document.createElement("div");
+maindiv.style.height = window.outerHeight + "px";
+maindiv.style.width = "0.5em";
+
+var text = document.createTextNode("Height: " + window.outerHeight);
+maindiv.appendChild(text);
+addToPage(maindiv);
+
+
+function addToPage(what) {
+    var attr = document.createAttribute("class");
+    attr.value = "vPSP";
+    what.setAttributeNode(attr);
+    document.body.appendChild(what);
+}
