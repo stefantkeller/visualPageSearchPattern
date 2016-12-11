82bec52ab86600d2fe0905e0106cb7dcb3012182
Stefan Keller <stefantkeller@gmail.com>
2016-12-11 16:01:42 +0100

    reorganize command order to be more readable

--- gui.js
+++ gui.js
@@ -1,16 +1,15 @@
-// add a div to the page, of class vPSP
-// its appearance we can control in the .css through this class name
+// add a div to the page, the height should correspond to the one of the open window
 var maindiv = document.createElement("div");
-maindiv.style.height = window.outerHeight + "px";
+maindiv.style.height = window.outerHeight + "px"; // this number we can't figure out in pure css, have to add it here dynamically
+document.body.appendChild(maindiv); // finally, ad to the page
 
+// maindiv's appearance we can control in the .css through this class name:
+var attr = document.createAttribute("class");
+attr.value = "vPSP";
+maindiv.setAttributeNode(attr);
+
+// to populate maindiv with content we use:
 var text = document.createTextNode("Height: " + window.outerHeight);
 maindiv.appendChild(text);
-addToPage(maindiv);
 
 
-function addToPage(what) {
-    var attr = document.createAttribute("class");
-    attr.value = "vPSP";
-    what.setAttributeNode(attr);
-    document.body.appendChild(what);
-}
